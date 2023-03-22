bf9e5ec1d21b14c2a6cf3221e7aaef715a953396
Martin B <m.bogdanov9110@gmail.com>
2023-03-22 17:23:56 +0200

    - updated the main layout navbar - updated css of navbar

--- next-blogger/app/layout.tsx
+++ next-blogger/app/layout.tsx
@@ -10,20 +10,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <nav className="navbar">
-          <div className="nav-container">
-            <Link className="home-logo" href="/">M.B. Space<span>.</span></Link>
-            <div className="nav-links-container" id="ftco-nav">
-              <ul className="nav-ul" role='list'>
-                <li className="nav-link"><Link href="/" className="nav-link">Home</Link></li>
-                <li className="nav-link"><Link href="/planets" className="nav-link">Planets</Link></li>
-                <li className="nav-link"><Link href="/interesting" className="nav-link">Interesting</Link></li>
-                <li className="nav-link"><Link href="/about" className="nav-link">About</Link></li>
-                <li className="nav-link"><Link href="/contact" className="nav-link">Contact</Link></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <div className="nav-container">
+          <nav className="navbar">
+            <ul className="nav-ul" role='list'>
+              <li className="nav-li"><Link href="/" className="nav-link">Home</Link></li>
+              <li className="nav-li"><Link href="/planets" className="nav-link">Planets</Link></li>
+              <li className="nav-li"><Link href="/interesting" className="nav-link">Interesting</Link></li>
+              <li className="nav-li"><Link href="/about" className="nav-link">About</Link></li>
+              <li className="nav-li"><Link href="/contact" className="nav-link">Contact</Link></li>
+            </ul>
+          </nav>
+        </div>
         {children}
       </body>
     </html>
