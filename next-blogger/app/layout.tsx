03e92ec6a3c73d34e0c64f30ae3a6f3a7712da88
Martin B <85784810+SpooRe91@users.noreply.github.com>
2023-03-28 12:02:10 +0000

    removed nav from main layout

--- next-blogger/app/layout.tsx
+++ next-blogger/app/layout.tsx
@@ -1,28 +1,26 @@
-import Link from 'next/link';
-import './globals.css';
+import "./globals.css";
+import Footer from "./common/Footer/layout";
+import Header from "./common/Header/layout";
 
 export const metadata = {
-  title: 'M.B. Blogger',
-  description: 'Blog site created by M.B.',
-}
+  title: "M.B. Blogger",
+  description: "Blog site created by M.B.",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
-        <div className="nav-container">
-          <nav className="navbar">
-            <ul className="nav-ul" role='list'>
-              <li className="nav-li"><Link href="/" className="nav-link">Home</Link></li>
-              <li className="nav-li"><Link href="/planets" className="nav-link">Planets</Link></li>
-              <li className="nav-li"><Link href="/interesting" className="nav-link">Interesting</Link></li>
-              <li className="nav-li"><Link href="/about" className="nav-link">About</Link></li>
-              <li className="nav-li"><Link href="/contact" className="nav-link">Contact</Link></li>
-            </ul>
-          </nav>
-        </div>
-        {children}
+        <main>
+          <Header />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
-  )
+  );
 }
--- next-blogger/app/page.tsx
+++ next-blogger/app/page.tsx
@@ -1,15 +1,14 @@
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles['main-container']}>
+      <div className={styles["main-container"]}>
         <h1>Space Blogger by M.B.</h1>
-
       </div>
     </main>
-  )
+  );
 }
