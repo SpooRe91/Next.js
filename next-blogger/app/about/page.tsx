71cb4304cea2c97fb45b6996d2296b797f21b0cb
Martin B <85784810+SpooRe91@users.noreply.github.com>
2023-03-28 12:02:47 +0000

    updated about

--- next-blogger/app/about/page.tsx
+++ next-blogger/app/about/page.tsx
@@ -1,13 +1,17 @@
 import "../globals.css";
-import Link from 'next/link';
-import styles from './styles.module.css';
+import Link from "next/link";
+import styles from "./styles.module.css";
+import Image from "next/image";
 
 export default function About() {
-    return (
-        <main className={styles.main}>
-            <h1> ABOUT </h1>
-            <p className={styles.description}>Here you can find a bit of information about M.B. and what he does.</p>
-            <Link href={'/'}>Back to home</Link>
-        </main>
-    )
-};
+  return (
+    <main className={styles.main}>
+      <h1> ABOUT </h1>
+
+      <p className={styles.description}>
+        Here you can find a bit of information about M.B. and what he does.
+      </p>
+      <Link href={"/"}>Back to home</Link>
+    </main>
+  );
+}
